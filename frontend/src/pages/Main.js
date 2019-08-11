deaaafd7d2de12ca9a9134fa172968a3390e500f
Ricardo Lima <phpricardo@gmail.com>
2019-08-11 17:13:51 -0300

    created front-end of app

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Main.css';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        }
+      })
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: match.params.id },
+    })
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDisLike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id },
+    })
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <div className="main-container">
+      <link to="/">
+        <img src={logo} alt="Tindev" />
+      </link>
+        { users.length > 0 ? (
+          <ul>
+            {users.map(user => (
+              <li key={user._id}>
+              <img src={user.avatar} alt={user.name}/>
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+    
+              <div className="buttons">
+                <button type="button" onClick={() => handleDisLike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button">
+                  <img src={like} alt="Like" onClick={() => handleLike(user._id)} />
+                </button>
+              </div>
+              </li>
+            ))}
+          </ul>
+        ) :  (
+          <div className="empty"> Não há mais ninguém para visualizar. :( </div>
+        )}
+    </div>
+  )
+}
